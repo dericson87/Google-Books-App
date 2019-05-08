0a7cbd57cc32334dd0e136ffa67cb5c02130a51b
Derek Ericson <derek.ericson@gmail.com>
2019-05-07 23:27:11 -0700

    app is functioning as needed for the assignment

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer fixed-bottom">
+		<div className = "container-fluid text-center">
+			<p>Project by: Derek Ericson</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
